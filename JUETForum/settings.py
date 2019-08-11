5c273b23860d4750d481dc8e089a80a2ca973007
Richesh Gupta <rgrichesh45@gmail.com>
2019-08-11 10:32:27 +0530

    pass changed

--- JUETForum/settings.py
+++ JUETForum/settings.py
@@ -137,6 +137,6 @@ LOGIN_URL           = 'login'
 EMAIL_HOST          = 'smtp.gmail.com'
 EMAIL_PORT          =  587
 EMAIL_HOST_USER     = 'pantomath.notice@gmail.com'
-EMAIL_HOST_PASSWORD = 'pantomath123'
+EMAIL_HOST_PASSWORD = 'pantomathkj123@'
 EMAIL_USE_TLS       = True
 EMAIL_USE_SSL       = False
